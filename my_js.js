7aeb610fc4afe02a6f46408bc888160323cedf73
henro <henrotanjung@gmail.com>
2023-11-18 02:19:42 +0700

    fix responsive of be part of us

--- my_js.js
+++ my_js.js
@@ -1,18 +1,18 @@
 function myFunction(x) {
-    var image_center_el = document.getElementById("image_center");
-    var image_last_el = document.getElementById("image_last");
+    var image_first_el = document.getElementById("image_first_col");
+    var image_last_el = document.getElementById("image_last_col");
     if (x.matches) { // If media query matches
         // document.body.style.backgroundColor = "yellow";
 
-        image_center_el.style.display = "none";
+        image_first_el.style.display = "none";
         image_last_el.style.display = "none";
     } else {
         // document.body.style.backgroundColor = "pink";
-        image_center_el.style.display = "flex";
-        image_last_el.style.display = "flex";
+        image_first_el.style.display = "block";
+        image_last_el.style.display = "block";
     }
 }
 
-var x = window.matchMedia("(max-width: 768px)")
+var x = window.matchMedia("(max-width: 767px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
